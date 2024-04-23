ed29f479383a447af5eff560ce6e92919fab2e72
JordanPostak <jordan_postak@hotmail.com>
2024-04-23 09:07:53 -0600

    W01 Code: Dynamic Arrays

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -33,14 +33,24 @@ public static class ArraysTester {
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     private static double[] MultiplesOf(double number, int length)
-    {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+{
+    // Create an empty list to store the multiples
+    List<double> multiplesList = new List<double>();
 
-        return new double[0]; // replace this return statement with your own
+    // Initialize a variable to store the current multiple, starting with the input number
+    double currentMultiple = number;
+
+    // Use a loop to iterate `length` times to generate the desired number of multiples
+    for (int i = 0; i < length; i++)
+    {
+        // In each iteration, multiply the current multiple by the input number and add it to the list
+        multiplesList.Add(currentMultiple);
+        currentMultiple += number; // Move to the next multiple
     }
+
+    // Return the list of multiples
+    return multiplesList.ToArray();
+}
     
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -50,12 +60,21 @@ public static class ArraysTester {
     /// <br /><br />
     /// Because a list is dynamic, this function will modify the existing <c>data</c> list rather than returning a new list.
     /// </summary>
-    private static void RotateListRight(List<int> data, int amount)
-    {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+   private static void RotateListRight(List<int> data, int amount)
+{
+    // Determine the actual amount of rotation needed by taking the modulo of the input amount
+    int rotationAmount = amount % data.Count;
 
-    }
+    // Create a temporary list to store the rotated elements
+    List<int> temp = new List<int>();
+
+    // Copy the last 'rotationAmount' elements from the original list to the temporary list
+    temp.AddRange(data.GetRange(data.Count - rotationAmount, rotationAmount));
+
+    // Remove the last 'rotationAmount' elements from the original list
+    data.RemoveRange(data.Count - rotationAmount, rotationAmount);
+
+    // Insert the elements from the temporary list at the beginning of the original list
+    data.InsertRange(0, temp);
+}
 }
